735ddebdc217bb36aea36bfd7709f314b2c34673
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-01-25 19:41:10 -0300

    Document the `NotSupportedException` class

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -1,3 +1,5 @@
+/// An exception thrown by the [SortedList] class when a not supported operation
+/// is executed
 class NotSupportedException implements Exception {
   final String message;
   NotSupportedException(this.message);
